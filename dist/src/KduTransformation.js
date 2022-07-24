60ccc4b4223a31db173d5fd6b345a007392a1da7
NKDuy <68154054+khanhduy1407@users.noreply.github.com>
2022-07-24 15:29:34 +0700

    Delete dist directory

--- dist/src/KduTransformation.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class KduTransformation {
-}
-exports.default = KduTransformation;
--- dist/src/astUtils.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Collection, ObjectMethod, ObjectExpression, FunctionExpression, ArrowFunctionExpression } from 'jscodeshift';
-import type { Context } from './wrapAstTransformation';
-declare type KduOptionsType = ObjectExpression | ArrowFunctionExpression | FunctionExpression | ObjectMethod;
-export declare function getKduOptions(context: Context): Collection<KduOptionsType>;
-export {};
